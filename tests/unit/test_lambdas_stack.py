a105831eda7edafc4228bfdf8fd31325e2e29d1a
Nick Olson <olsonnick10@yahoo.com>
2022-04-19 20:21:01 -0700

    Removed stack source files (CDK synth misses them)

--- tests/unit/test_lambdas_stack.py
+++ tests/unit/test_lambdas_stack.py
@@ -1,13 +1,13 @@
 import aws_cdk as core
 import aws_cdk.assertions as assertions
 
-from wow.wow_stack import WowStack
+from app import NlpLambdaStack
 
 # example tests. To run these tests, uncomment this file along with the example
 # resource in wow/wow_stack.py
 def test_sqs_queue_created():
     app = core.App()
-    stack = WowStack(app, "wow")
+    stack = NlpLambdaStack(app, "AiidNlpStack")
     template = assertions.Template.from_stack(stack)
 
 #     template.has_resource_properties("AWS::SQS::Queue", {
