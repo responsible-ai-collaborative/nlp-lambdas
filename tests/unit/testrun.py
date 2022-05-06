a0a1b02a578848a95a12e8c145deca0e6b246dd4
Nicholas Broce <broceni@oregonstate.edu>
2022-05-05 18:15:43 -0700

    Co-authored-by: Nick Olson <olsonnick10@yahoo.com>

--- tests/unit/testrun.py
+++ tests/unit/testrun.py
@@ -27,18 +27,55 @@ def runTest(expectIncident, docsJson):
         print(bestID)
     return(expectIncident == bestID)
  
- 
-# Initialize parser
-parser = argparse.ArgumentParser()
- 
-# Adding optional argument
-parser.add_argument("-i", "--ExpectIncidentNumber", type = int, help = "Give an Expect Incident Id number")
-parser.add_argument("-d", "--DocsJson", help = "Give a .\docs .json file path")
- 
-# Read arguments from command line
-args = parser.parse_args()
+# inputs are expectIncident number and a .\docs .json file path
+#   (runs w/o redirecting output and err to files)
+def runTestPipeTest(expectIncident, docsJsonPath):
+    # Define command for subprocess
+    cmd = ['sam', 'local', 'invoke','similar', '-t', '.\cdk.out\AiidNlpLambdaStack.template.json', '-e', docsJsonPath]
 
-if args.ExpectIncidentNumber and args.DocsJson:
-    print(runTest(args.ExpectIncidentNumber, args.DocsJson))
+    # Spawn subprocess and wait for its complete stdout
+    with open("stderr.txt","wb") as err:
+        p = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
+        stdout = p.communicate()[0]
 
+    # Wait for completion
+    while p.poll() == None: continue
 
+    # Normal execution
+    if (p.poll() == 0):
+        print(f'Stdout: "{stdout}"')
+        data = {}
+        try:
+            data = json.loads(json.loads(stdout))
+        except:
+            print('Failure in json parsing')
+
+        try:
+            if(data["statusCode"] == 200):
+                listoftupals = ast.literal_eval(data['body']['msg'])
+                bestID = listoftupals[0][1]
+                print(f'bestID output: {bestID}')
+        except:
+            print('Error in reading SAM output')
+    else:
+        print(f'Error in SAM execution, exit code: {p.poll()}')
+
+    return(expectIncident == bestID)
+
+def main():
+    # Initialize parser
+    parser = argparse.ArgumentParser()
+    
+    # Adding optional argument
+    parser.add_argument("-i", "--ExpectIncidentNumber", type = int, help = "Give an Expect Incident Id number")
+    parser.add_argument("-d", "--DocsJson", help = "Give a .\docs .json file path")
+    
+    # Read arguments from command line
+    args = parser.parse_args()
+
+    if args.ExpectIncidentNumber and args.DocsJson:
+        return runTestPipeTest(args.ExpectIncidentNumber, args.DocsJson)
+        # return runTest(args.ExpectIncidentNumber, args.DocsJson)
+
+if __name__ == "__main__":
+   main()
