cc1d986e4a502f6e40401053bc6e170c65a4f675
Nicholas Broce <broceni@oregonstate.edu>
2022-05-08 13:16:35 -0700

    Created several testing documents for use in apitest.py and testrun.py, cleaned up testrun.py and formatted apitest.py

--- tests/testrun.py
+++ tests/testrun.py
@@ -3,6 +3,7 @@ import json
 import ast
 import argparse
 import sys
+import time
 
 # inputs are expectIncident number and a .\docs .json file path
 def runTest(expectIncident, docsJson):
@@ -35,17 +36,14 @@ def runTestPipeTest(expectIncident, docsJsonPath):
     cmd = ['sam', 'local', 'invoke', 'similar', '-t', './cdk.out/AiidNlpLambdaStack.template.json', '-e', docsJsonPath]
 
     # Spawn subprocess and wait for its complete stdout (depending on DefaultShell argument)
-    if (defaultShell):
-        print(cmd)
-        p = subprocess.Popen(' '.join(cmd), stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
-    else:
-        print((['bash', '-c', '\"']+cmd+['\"']))
-        p = subprocess.Popen(' '.join(['bash', '-c', '\"']+cmd+['\"']), stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=False)
+    print(cmd)
+    p = subprocess.Popen(' '.join(cmd), stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
     
     stdout, err = p.communicate()
+    time.sleep(15)
 
-    print(f'stdout: "{stdout}"')
-    print(f'stderr: "{err}"')
+    print("stdout: ", json.loads(stdout))
+    print("stderr: ", err)
 
     # Wait for completion
     while p.poll() == None: continue
@@ -63,29 +61,24 @@ def runTestPipeTest(expectIncident, docsJsonPath):
             if(data["statusCode"] == 200):
                 listoftupals = ast.literal_eval(data['body']['msg'])
                 bestID = listoftupals[0][1]
-                print(f'bestID output: {bestID}')
+                print("bestID output: ", bestID)
         except:
-            raise Exception('Error in reading SAM output, stderr: "{err}"')
+            raise Exception("Error in reading SAM output, stderr: ", err)
     else:
-        raise Exception(f'Error in SAM execution, exit code: {p.poll()}')
+        raise Exception("Error in SAM execution, exit code: ", p.poll())
 
     return(expectIncident == bestID)
 
 def main():
-    # Use global defaultShell
-    global defaultShell
-
     # Initialize parser
     parser = argparse.ArgumentParser()
     
     # Adding optional argument
     parser.add_argument("-i", "--ExpectIncidentNumber", type = int, help = "Give an Expect Incident Id number")
     parser.add_argument("-d", "--DocsJson", help = "Give a .\docs .json file path")
-    parser.add_argument("--DefaultShell", action = "store_true", help = "Signal that you want to use your default shell rather than bash")
     
-    # Read arguments from command line
+    # Read arguments from command lineW
     args = parser.parse_args()
-    defaultShell = args.DefaultShell
 
     if args.ExpectIncidentNumber and args.DocsJson:
         sys.exit(not runTestPipeTest(args.ExpectIncidentNumber, args.DocsJson))
--- tests/unit/apitest.py
+++ tests/unit/apitest.py
@@ -2,18 +2,48 @@ import requests
 import ast
 import subprocess
 import json
+import argparse
 import time
+import sys
 
-# start the api
-cmd = ['sam', 'local', 'start-api', '-t', '.\cdk.out\AiidNlpLambdaStack.template.json']
+def apiTestRun(expectIncident, docsJsonPath):
+     # start the api
+        cmd = ['sam', 'local', 'start-api', '-t', '.\cdk.out\AiidNlpLambdaStack.template.json']
 
-with open("stdout.json","wb") as out, open("stderr.txt","wb") as err:
-    p = subprocess.Popen(cmd,stdout=out,stderr=err, shell=True)
-    time.sleep(10)
+        p = subprocess.Popen(' '.join(cmd), shell=True)
+        time.sleep(10)
 
-    res = requests.get("http://127.0.0.1:3000/similar?text=\"wow\"")
-    print(ast.literal_eval(json.loads(res.text)['body']['msg'])[0][1])
+        with open(docsJsonPath, encoding="utf-8") as json_file:
+            json_payload = json.load(json_file)
+            get_payload = json_payload['text']
+            res = requests.post("http://127.0.0.1:3000/similar", json=json_payload)
+            postOutputIncident = ast.literal_eval(json.loads(res.text)['body']['msg'])[0][1]
+            print(postOutputIncident)
 
-    res = requests.post("http://127.0.0.1:3000/similar", json={"text":"wow", "num":"3"})
-    print(ast.literal_eval(json.loads(res.text)['body']['msg'])[0][1])
-    p.kill()
+            get_url = ("http://127.0.0.1:3000/similar?text=\\\"" + get_payload + "\\\"")
+            print(get_url)
+            res = requests.get(get_url)
+            getOutputIncident = ast.literal_eval(json.loads(res.text)['body']['msg'])[0][1]
+            print(getOutputIncident)
+
+        p.kill()
+        return(getOutputIncident == postOutputIncident == expectIncident)
+
+
+def main():
+     # Initialize parser
+    parser = argparse.ArgumentParser()
+    
+    # Adding optional argument
+    parser.add_argument("-i", "--ExpectIncidentNumber", type = int, help = "Give an Expect Incident Id number")
+    parser.add_argument("-d", "--DocsJson", help = "Give a .\docs .json file path")
+    
+    # Read arguments from command lineW
+    args = parser.parse_args()
+
+    if args.ExpectIncidentNumber and args.DocsJson:
+        sys.exit(not apiTestRun(args.ExpectIncidentNumber, args.DocsJson))
+
+
+if __name__ == "__main__":
+   main()
