865d9d3b7c2a79231ad5aa99789fbdd06ead22d4
Nick Olson <olsonnick10@yahoo.com>
2022-05-23 18:48:56 -0700

    Eliminate "lamdba" misspelling once and for all

--- tests/unit/test_lambda_execution.py
+++ tests/unit/test_lambda_execution.py
@@ -18,35 +18,35 @@ def mkpath(json_filename: str):
 
 def test_good_text_to_db_similar_execution():
     assert testrun.run_lambda_tests(1, mkpath("lambda_test_request_incident_1.json"), lambdaName="text-to-db-similar")
-    assert testrun.run_lambda_tests(15, mkpath("lamdba_test_request_incident_15.json"), lambdaName="text-to-db-similar")
-    assert testrun.run_lambda_tests(10, mkpath("lamdba_test_request_incident_10.json"), lambdaName="text-to-db-similar")
+    assert testrun.run_lambda_tests(15, mkpath("lambda_test_request_incident_15.json"), lambdaName="text-to-db-similar")
+    assert testrun.run_lambda_tests(10, mkpath("lambda_test_request_incident_10.json"), lambdaName="text-to-db-similar")
     
 def test_good_embed_to_db_similar_execution():
     assert testrun.run_lambda_tests(1, mkpath("lambda_test_request_incident_1_embedding.json"), lambdaName="embed-to-db-similar")
-    assert testrun.run_lambda_tests(15, mkpath("lamdba_test_request_incident_15_embedding.json"), lambdaName="embed-to-db-similar")
-    assert testrun.run_lambda_tests(10, mkpath("lamdba_test_request_incident_10_embedding.json"), lambdaName="embed-to-db-similar")
+    assert testrun.run_lambda_tests(15, mkpath("lambda_test_request_incident_15_embedding.json"), lambdaName="embed-to-db-similar")
+    assert testrun.run_lambda_tests(10, mkpath("lambda_test_request_incident_10_embedding.json"), lambdaName="embed-to-db-similar")
 
 def test_incorrect_text_to_db_similar_value():
-    assert not testrun.run_lambda_tests(-5, mkpath("lamdba_test_request_incident_15.json"), lambdaName="text-to-db-similar")
+    assert not testrun.run_lambda_tests(-5, mkpath("lambda_test_request_incident_15.json"), lambdaName="text-to-db-similar")
 
 def test_incorrect_embed_to_db_similar_value():
-    assert not testrun.run_lambda_tests(-5, mkpath("lamdba_test_request_incident_15_embedding.json"), lambdaName="embed-to-db-similar")
+    assert not testrun.run_lambda_tests(-5, mkpath("lambda_test_request_incident_15_embedding.json"), lambdaName="embed-to-db-similar")
 
 def test_json_input_syntax_error_text_to_db_similar_():
     with pytest.raises(JsonException):
-        testrun.run_lambda_tests(1, mkpath("lamdba_bad_test_request_bad_syntax.json"), lambdaName="text-to-db-similar")
+        testrun.run_lambda_tests(1, mkpath("lambda_bad_test_request_bad_syntax.json"), lambdaName="text-to-db-similar")
 
 def test_json_input_syntax_error_embed_to_db_similar_():
     with pytest.raises(JsonException):
-        testrun.run_lambda_tests(1, mkpath("lamdba_bad_test_request_bad_syntax.json"), lambdaName="embed-to-db-similar")
+        testrun.run_lambda_tests(1, mkpath("lambda_bad_test_request_bad_syntax.json"), lambdaName="embed-to-db-similar")
 
 def test_missing_required_input_field_text_to_db_similar_():
     with pytest.raises(SamOutputException):
-        testrun.run_lambda_tests(1, mkpath("lamdba_bad_test_request_missing_input.json"), lambdaName="text-to-db-similar")
+        testrun.run_lambda_tests(1, mkpath("lambda_bad_test_request_missing_input.json"), lambdaName="text-to-db-similar")
         
 def test_missing_required_input_field_embed_to_db_similar_():
     with pytest.raises(SamOutputException):
-        testrun.run_lambda_tests(1, mkpath("lamdba_bad_test_request_missing_input.json"), lambdaName="embed-to-db-similar")
+        testrun.run_lambda_tests(1, mkpath("lambda_bad_test_request_missing_input.json"), lambdaName="embed-to-db-similar")
 
 # Additional tests might include
 #   - checking that bestOf=N parameter correctly gives N results
