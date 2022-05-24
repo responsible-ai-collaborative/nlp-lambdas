bb4ef439a4b3dfbcf08102780d9eba219b5e1dc3
Nick Olson <olsonnick10@yahoo.com>
2022-05-24 15:17:57 -0700

    Added try/except for literal_eval

--- inference/embed-to-db-similar.py
+++ inference/embed-to-db-similar.py
@@ -171,14 +171,14 @@ def handler(event, context):
             f'Zero results requested with the "num" value of 0. Use value <0 for maximum possible.')
 
     # Handle unicode in event_text and parse it to a list
-    embed:list = literal_eval(unidecode(embed_text))
-    # try:
-    #     embed:list = literal_eval(unidecode(embed_text))
-    # except:
-    #     result['statusCode'] = 500
-    #     result['body'] = {'msg': 'Error during literal evaluation of embed_text!'}
-    #     result['headers']['Content-Type'] = "application/json"
-    #     return json.dumps(result)
+    # embed:list = literal_eval(unidecode(embed_text))
+    try:
+        embed:list = literal_eval(unidecode(embed_text))
+    except:
+        result['statusCode'] = 500
+        result['body'] = {'msg': 'Error during literal evaluation of embed_text!'}
+        result['headers']['Content-Type'] = "application/json"
+        return json.dumps(result)
 
     # Found event_text, use it and return result
     try:
