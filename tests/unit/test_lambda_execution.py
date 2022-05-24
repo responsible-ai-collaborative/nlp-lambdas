a6d7899d87600e20c231f8892c1eba88fcdc953e
Nick Olson <olsonnick10@yahoo.com>
2022-05-23 17:15:23 -0700

    Renamed runTestPipeTest to run_text_to_db_similar_tests

--- tests/unit/test_lambda_execution.py
+++ tests/unit/test_lambda_execution.py
@@ -17,20 +17,20 @@ def mkpath(json_filename: str):
                         json_filename)
 
 def test_good_execution():
-    assert testrun.runTestPipeTest(1, mkpath("lambda_test_request_incident_1.json"))
-    assert testrun.runTestPipeTest(15, mkpath("lamdba_test_request_incident_15.json"))
-    assert testrun.runTestPipeTest(10, mkpath("lamdba_test_request_incident_10.json"))
+    assert testrun.run_text_to_db_similar_tests(1, mkpath("lambda_test_request_incident_1.json"))
+    assert testrun.run_text_to_db_similar_tests(15, mkpath("lamdba_test_request_incident_15.json"))
+    assert testrun.run_text_to_db_similar_tests(10, mkpath("lamdba_test_request_incident_10.json"))
 
 def test_incorrect_value():
-    assert not testrun.runTestPipeTest(-5, mkpath("lamdba_test_request_incident_15.json"))
+    assert not testrun.run_text_to_db_similar_tests(-5, mkpath("lamdba_test_request_incident_15.json"))
 
 def test_json_input_syntax_error():
     with pytest.raises(JsonException):
-        testrun.runTestPipeTest(1, mkpath("lamdba_bad_test_request_bad_syntax.json"))
+        testrun.run_text_to_db_similar_tests(1, mkpath("lamdba_bad_test_request_bad_syntax.json"))
 
 def test_missing_required_input_field():
     with pytest.raises(SamOutputException):
-        testrun.runTestPipeTest(1, mkpath("lamdba_bad_test_request_missing_input.json"))
+        testrun.run_text_to_db_similar_tests(1, mkpath("lamdba_bad_test_request_missing_input.json"))
 
 # Additional tests might include
 #   - checking that bestOf=N parameter correctly gives N results
