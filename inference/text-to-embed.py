fb415573ab3eb0e7b11098834c9936efca8b5914
Luna McNulty <l.mcnulty@riseup.net>
2022-07-22 10:06:06 -0400

    Add local mocking, change response format

--- inference/text-to-embed.py
+++ inference/text-to-embed.py
@@ -2,20 +2,27 @@
 from ast import literal_eval
 import os
 import json
-import os
 import pandas as pd
 import torch
 from transformers import LongformerTokenizer, LongformerModel
 from unidecode import unidecode
 import pandas as pd
 from typing import Union
+from sys import argv, stdin
+from hashlib import sha1
+
+model_location = (
+    os.path.dirname(os.path.realpath(__file__)) + os.path.sep + 'model'
+    if '--local' in argv
+    else '/function/model'
+)
 
 # Load tokenizer and model from local, pretrained model files
-tokenizer = LongformerTokenizer.from_pretrained('/function/model',
-                                                local_files_only=True,
-                                                model_max_length=2000)
-model = LongformerModel.from_pretrained('/function/model',
-                                        local_files_only=True)
+tokenizer = LongformerTokenizer.from_pretrained( model_location,
+                                                 local_files_only=True,
+                                                 model_max_length=2000 )
+
+model = LongformerModel.from_pretrained(model_location, local_files_only=True)
 
 # Get longformer embedding
 def get_embedding(text:str):
@@ -54,9 +61,12 @@ def handler(event, context):
 
     # Found event_text, use it and return result
     try:
-        res = get_embedding(event_text)
         result['statusCode'] = 200
-        result['body']['msg'] = str(res)
+        result['msg'] = 'Success'
+        result['body']['embedding'] = {
+          'vector': get_embedding(event_text).detach().tolist(),
+          'from_text_hash': sha1(event_text.encode('utf-8')).hexdigest()
+        }
         result['headers']['Content-Type'] = "application/json"
     except:
         result['statusCode'] = 500
@@ -64,3 +74,5 @@ def handler(event, context):
         result['headers']['Content-Type'] = "application/json"
     return json.dumps(result)
 
+if '--local' in argv:
+    print(handler({'body': json.dumps({'text': stdin.read()})}, None))
